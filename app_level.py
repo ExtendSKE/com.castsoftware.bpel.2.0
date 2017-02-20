563081c066395ff161e2f95896926b0db8a1e24e
Mukul Saini <MSA@corp.castsoftware.com>
2017-02-20 14:16:29 +0530

    code refactoring

--- app_level.py
+++ app_level.py
@@ -8,7 +8,6 @@ class ExtensionApplication(cast.application.ApplicationLevelExtension):
 
     def end_application(self, application):
         logging.debug("running code at the end of an application")
-        logging.debug("*************")
         list1 = []
         for line in self.get_intermediate_file('file.txt'):
             list1.append(line)
@@ -18,26 +17,16 @@ class ExtensionApplication(cast.application.ApplicationLevelExtension):
         resourceReferences = list(application.search_objects(category='BPEL_Invoke', load_properties=False))
         resourceReferences1 = list(application.search_objects(category='JV_METHOD', load_properties=False))
         for i in resourceReferences1 :
-        #if i.get_type() == "JV_METHOD" :
             for j in list2 :
                 for k in j :
-                    #logging.debug(j[k].find( i.get_name()))
-                    #if j[k].find( i.get_name()) != -1 :
                     if (re.search('(?:\w+\.'+i.get_name()+'\(|\w+\.'+i.get_name()+'\s+\()', j[k])) : 
                         for y in resourceReferences :
                             #logging.debug( y.get_fullname())
                             if '--' in y.get_fullname():
-                                #logging.debug(str(k))
                                 if str(k) == y.get_fullname().split("--")[1] :
                                     cast.application.create_link("callLink",y,i,bookmark=None)
                                     logging.debug( y.get_fullname())
                         #logging.debug("Invoke name : "+ str(k))
                         #logging.debug("java object name"+ str(i))
 
-
-
-            '''
-            if i.name == "name":
-                cast.application.create_link("callLink",resourceReferences[0],i,bookmark=None)
-            '''
-            logging.debug("DONE!")
+        logging.debug("DONE!")
--- extractJavaCode.py
+++ extractJavaCode.py
@@ -1,29 +1,20 @@
 import xml.etree.ElementTree as ET
 import re
 
-
-def parse_xml(root,list1):
-	if root is None:
-		return
-	for i in root:
-		data =str(i.tag)
-		data =re.sub('{.*?}', '',data)
-		if data == "invoke":
-			print("New")
-			for k in list(i.iter()) :
-				data1 =re.sub('{.*?}', '',str(k.tag)) 
-				if data1 == "invoke" :
-					l = k
-				if data1 == "javaCode" :
-					print (l.attrib["name"])
-					print (k.text)
-					dict = { l.attrib["name"] : k.text}		
-					list1.append(dict)
-		parse_xml(i,list1)
+def parse_xml(root, list1):
+	for k in list(root.iter()) :
+		data1 =re.sub('{.*?}', '',str(k.tag)) 
+		if data1 == "invoke" :
+			l = k
+		if data1 == "javaCode" :
+			print (l.attrib["name"])
+			print (k.text)
+			dict = { l.attrib["name"] : k.text}		
+			list1.append(dict)
+            
 def getInvokeJavaCode(file) : 
-	list1 = []
 	tree = ET.parse(file)
 	root =tree.getroot()
-	parse_xml(root,list1)
+	list1 = []
+	parse_xml(root, list1)
 	return list1
-
